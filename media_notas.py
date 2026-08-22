79200f5527b1d5ded84aa4b0903d628367c7058d
Hanyssa <hanyssamkt@gmail.com>
2026-08-22 15:25:31 -0300

    Add approval message based on average score
    Calculate average and provide approval message based on it.

--- media_notas.py
+++ media_notas.py
@@ -1,4 +1,10 @@
 n1=float(input('Digite uma nota:'))
 n2=float(input('Digite outra nota:'))
-s=(n1+n2)/2
+n3=(n1+n2)/2
 print('A sua média é {}'.format(s))
+
+
+if n3 >=7.0:
+    print('Parabéns, sua nota foi {}, está aprovado'.format(n3))
+else:
+    print('Sua nota foi {}, reprovado'.format(n3))
